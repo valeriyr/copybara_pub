bed8a6ea5512c565b58dc83934996b32adc786dc
Valerii Reutov <valerii.reutov@iota.org>
2023-12-11 16:36:51 +0200

    more conflict changes

--- pub-types/src/lib.rs
+++ pub-types/src/lib.rs
@@ -1,4 +1,4 @@
-pub fn add2(left: usize, right: usize) -> usize {
+pub fn add(left: u32, right: u32) -> u32 {
     left + right
 }
 
@@ -8,7 +8,7 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let result = add2(2, 2);
+        let result = add(2, 2);
         assert_eq!(result, 4);
     }
 }
